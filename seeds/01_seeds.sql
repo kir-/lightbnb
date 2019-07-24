c5fb4eb9177a2dc756a8354ee23a966cdd729d9c
kir- <graysonclee@gmail.com>
2019-07-24 23:56:57 +0000

    added data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password) VALUES 
+('John', 'John@doe.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
+('Sarah', 'Sarah@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
+('Tom', 'Tom@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Bob', 'Bob@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Adeline', 'Adeline@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
